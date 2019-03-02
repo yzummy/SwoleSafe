bcff983ebad678dd3c2dcaff0d3ecd35726f3341
gajankumar1016 <gajankumar1016@gmail.com>
2019-03-02 16:22:26 -0600

    Added more fields to user

--- app/dbcreate.py
+++ app/dbcreate.py
@@ -6,20 +6,27 @@ if __name__ == '__main__':
 
     bp1 = BodyPart(name="chest")
     bp2 = BodyPart(name="bicep")
-    db.session.add(bp1)
-    db.session.add(bp2)
+    bp3 = BodyPart(name='quadriceps')
+    bp4 = BodyPart(name='hamstring')
+    bp5 = BodyPart(name='deltoids')
+    db.session.add_all([bp1, bp2, bp3, bp4, bp5])
 
-    e1 = Exercise(name="Bicep Curl", description="Contract bicep while holding weight then release")
+    e1 = Exercise(name="Bicep Curl", description="Contract bicep while holding weight then release slowly")
     e1.uses.append(bp2)
-    e2 = Exercise(name="Push up", description="Everyone knows what push up is")
-    e2.uses.append(bp1)
-    db.session.add(e1)
-    db.session.add(e2)
 
-    u1 = User(username="ArnoldSchwazzaneger123")
+    e2 = Exercise(name="Front Raise", description="Raise dumbells in front of you")
+    e2.uses.append(bp5)
+
+    e3 = Exercise(name="Squat", description="Pretend like you are sitting down and getting up from an invisible chair")
+    e3.uses.extend([bp3, bp4])
+
+
+    db.session.add_all([e1, e2, e3])
+
+    u1 = User(username="ArnoldSchwazzaneger123", password="abc", level="advanced")
     u1.targets.append(bp1)
 
-    u2 = User(username="DwanedaRockJohnson")
+    u2 = User(username="DwanedaRockJohnson", password="123", level="beginner")
     u2.targets.extend([bp1, bp2])
 
     db.session.add_all([u1, u2])
--- app/main.py
+++ app/main.py
@@ -15,11 +15,10 @@ db = SQLAlchemy(app)
 
 ma = Marshmallow(app)
 
-
 targets = db.Table('targets',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('bodypart_id', db.Integer, db.ForeignKey('bodypart.id'), primary_key=True)
-                )
+                   db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+                   db.Column('bodypart_id', db.Integer, db.ForeignKey('bodypart.id'), primary_key=True)
+                   )
 
 uses = db.Table('uses',
                 db.Column('bodypart_id', db.Integer, db.ForeignKey("bodypart.id"), primary_key=True),
@@ -31,10 +30,11 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    # email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
-    targets = db.relationship('BodyPart', secondary=targets, lazy='subquery',
-                              backref=db.backref('users', lazy=True))
+    password = db.Column(db.String(120), nullable=False)
+    level = db.Column(db.String(30))
+    # password_hash = db.Column(db.String(128))
+    targets = db.relationship('BodyPart', secondary=targets, lazy='dynamic',
+                              backref=db.backref('users', lazy='dynamic'))
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -52,15 +52,18 @@ class Exercise(db.Model):
     name = db.Column(db.String(64), index=True, unique=True)
     description = db.Column(db.String(250))
     uses = db.relationship('BodyPart', secondary=uses, lazy='subquery',
-                              backref=db.backref('exercise', lazy=True))
+                           backref=db.backref('exercise', lazy=True))
+
 
 class BodyPartSchema(ma.ModelSchema):
     class Meta:
         model = BodyPart
 
+
 class UserSchema(ma.ModelSchema):
     class Meta:
         model = User
+        exclude = ("password",)
 
 
 class ExerciseSchema(ma.ModelSchema):
@@ -84,7 +87,7 @@ class UsersResource(Resource):
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('username')
-        # parser.add_argument('email')
+        parser.add_argument('password')
         parser.add_argument('bodypart', type=str, action='append')
         args = parser.parse_args()
 
@@ -92,7 +95,7 @@ class UsersResource(Resource):
         if user_exists:
             return "User already exists", 409
 
-        new_user = User(username=args['username'])
+        new_user = User(username=args['username'], password=args['password'])
         for bp in args['bodypart']:
             print(bp)
             new_bp = BodyPart.query.filter_by(name=bp.lower()).first()
@@ -119,11 +122,23 @@ class ExercisesResource(Resource):
         exercises_schema = ExerciseSchema(many=True)
         return exercises_schema.jsonify(exercises)
 
+    def post(self):
+        return "In post method of Exercises Resource"
+
+
+class UserExercisesResource(Resource):
+    # def get(self, username):
+    #     user = User.query.filter_by(username=username).first()
+    #     targets = []
+    #     for target_id in user.targets:
+    #         for ex in Exercise.query.all():
+    #             for
+    pass
+
 
 api.add_resource(UserResource, '/user/<user_id>')
 api.add_resource(UsersResource, '/users/')
 api.add_resource(ExercisesResource, '/exercises/')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
