e9ffb93b16d6f9bc7c48c808fd320656d9d15b27
gajankumar1016 <gajankumar1016@gmail.com>
2019-03-02 23:35:25 -0600

    Created better algorithm for generating workout routine

--- app/dbcreate.py
+++ app/dbcreate.py
@@ -18,16 +18,16 @@ if __name__ == '__main__':
 
     db.session.add_all([bp1, bp2, bp3, bp4, bp5, bp6, bp7, bp8, bp9, bp10, bp11])
 
-    e1 = Exercise(name="Bicep Curl", description="Contract bicep while holding weight then release slowly")
+    e1 = Exercise(name="Bicep Curl", base_reps=8, rep_incr=4, description="Contract bicep while holding weight then release slowly")
     e1.uses.append(bp2)
 
-    e2 = Exercise(name="Front Raise", description="Raise dumbells in front of you")
+    e2 = Exercise(name="Front Raise", base_reps=10, rep_incr=7, description="Raise dumbells in front of you")
     e2.uses.append(bp5)
 
-    e3 = Exercise(name="Squat", description="Pretend like you are sitting down and getting up from an invisible chair")
+    e3 = Exercise(name="Squat", base_reps=15, rep_incr=15, description="Pretend like you are sitting down and getting up from an invisible chair")
     e3.uses.extend([bp3, bp4, bp8])
 
-    e4 = Exercise(name="Pushup", description="Keep your back flat. Remember to breathe.")
+    e4 = Exercise(name="Pushup", base_reps = 10, rep_incr=12, description="Keep your back flat. Remember to breathe.")
     e4.uses.extend([bp1, bp5, bp6, bp11])
 
 
